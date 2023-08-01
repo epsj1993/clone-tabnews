67e08ba503a55c0d26b2ab2c8580ab9625e02db1
Edmilson P S Junior <140121593+epsj1993@users.noreply.github.com>
2023-08-01 23:16:12 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinando para criar um site do zero
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-function Home(){
-    return <h1>
-        Se você já deu a bundinha da uma risadinha!
-    </h1>
+function Home() {
+  return <h1>Se você já deu a bundinha da uma risadinha!</h1>;
 }
-
 export default Home;
